f163187be3082c12b903fbe584ba4acb946ec34b
Leonardo Ferreira <leonardo.ferreira01@fatec.sp.gov.br>
2023-04-26 12:00:06 -0300

    Create 001-JanelaBasica.py

---
+++ 001-JanelaBasica.py
@@ -0,0 +1,14 @@
+import tkinter as tk
+
+#Criando uma janela
+janela = tk.Tk()
+
+#Definir um titulo 
+janela.title("Controle de Estoque")
+
+
+#Definir dimensões da janela
+janela.geometry("400x300")
+
+#Inicia o loop principal da janela
+janela.mainloop()
